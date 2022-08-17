086a76fe72078694147ba49c7dd8090104fc6afa
Manezeu Patricia <pmanezeu@gmail.com>
2022-08-17 14:57:51 +0100

    Update on request

--- queries.sql
+++ queries.sql
@@ -5,7 +5,7 @@ SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
 
 
-SELECT name FROM animals WHERE neutered = true AND  escape_attempts >= 3;
+SELECT name FROM animals WHERE neutered = true AND  escape_attempts < 3;
 
 
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR  name = 'Pikachu';
