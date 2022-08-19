cc0e3cc69cd3a7df6fc80d92ad280ff663e0a10b
Manezeu Patricia <pmanezeu@gmail.com>
2022-08-19 02:03:15 +0100

    Added many-to-many tables, added datas and querries

--- queries.sql
+++ queries.sql
@@ -51,3 +51,22 @@ SELECT animals.name AS Jennifers_digimons FROM animals LEFT JOIN owners on owner
 SELECT name as Dean_loyal_animals FROM animals LEFT JOIN owners ON owner_id = owners.id WHERE owners.full_name = 'Dean Winchester' AND escape_attempts = 0;
 
 SELECT full_name AS owner, COUNT(name) AS total_animals FROM animals RIGHT JOIN owners ON owner_id = owners.id GROUP BY owner ORDER BY total_animals DESC LIMIT 1;
+
+
+SELECT animals.name AS william_last_animal_seen, visit_date AS on_the FROM visits LEFT JOIN animals ON animal_id = animals.id LEFT JOIN vets ON vet_id = vets.id WHERE vets.name = 'William Tatcher' ORDER BY visit_date DESC LIMIT 1;
+
+SELECT DISTINCT COUNT(animal_id) AS mendez_total_diff_animals_seen FROM visits LEFT JOIN vets ON vet_id = vets.id WHERE vets.name = 'Stephanie Mendez';
+
+SELECT vets.name AS vet,  string_agg(species.name, ', ') AS speciality FROM specializations INNER JOIN species ON species_id = species.id RIGHT JOIN vets ON vet_id = vets.id GROUP BY vet;
+
+SELECT animals.name AS animal, visit_date FROM visits INNER JOIN animals ON animal_id = animals.id INNER JOIN vets ON vet_id = vets.id WHERE vets.name = 'Stephanie Mendez' AND visit_date BETWEEN '2020-04-01' AND '2020-08-30';
+
+SELECT animals.name AS animal, COUNT(vet_id) AS visits_to_vet FROM visits INNER JOIN animals ON animal_id = animals.id GROUP BY animal ORDER BY visits_to_vet DESC LIMIT 1;
+
+SELECT animals.name AS maisy_first_visit, visit_date FROM visits INNER JOIN animals ON animal_id = animals.id INNER JOIN vets ON vet_id = vets.id WHERE vets.name = 'Maisy Smith' ORDER BY visit_date ASC LIMIT 1;
+
+SELECT animals.name AS animal_name, animals.date_of_birth, animals.escape_attempts, animals.neutered, animals.weight_kg, vets.name AS vet_name, vets.age AS vet_age, vets.date_of_graduation AS vet_date_of_graduation, visit_date FROM visits INNER JOIN animals ON animal_id = animals.id INNER JOIN vets ON vet_id = vets.id ORDER BY visit_date LIMIT 1;
+
+SELECT SUM(result.unspec_animals_count) AS total_unspecialized_visits FROM (SELECT vets.name AS vet_name, COUNT(animal_id) AS unspec_animals_count FROM visits INNER JOIN vets ON visits.vet_id = vets.id INNER JOIN animals ON visits.animal_id = animals.id FULL JOIN specializations ON vets.id = specializations.vet_id WHERE animals.species_id NOT IN (SELECT species_id FROM specializations WHERE vet_id = vets.id)  GROUP BY vet_name) AS result;
+
+SELECT species.name AS maisy_species_visits, COUNT(animal_id) AS species_visits_count FROM visits INNER JOIN animals ON animal_id = animals.id INNER JOIN species ON animals.species_id = species.id INNER JOIN vets on vet_id = vets.id WHERE vets.name = 'Maisy Smith' GROUP BY maisy_species_visits ORDER BY species_visits_count DESC LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -29,3 +29,23 @@ ALTER TABLE animals ADD species_id int;
 ALTER TABLE animals ADD owner_id int;
 ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    age int,
+    date_of_graduation date
+);
+
+CREATE TABLE specializations (
+    species_id int REFERENCES species (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    vet_id int REFERENCES vets (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT specializations_pkey PRIMARY KEY (species_id, vet_id)
+);
+
+CREATE TABLE visits (
+    id SERIAL PRIMARY KEY,
+    animal_id int REFERENCES animals (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    vet_id int REFERENCES vets (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    visit_date date
+);
